dd9a234eb7af07af0c16a679da88c7a10f08f558
ranjiterao <ranjiterao@gmail.com>
2015-12-07 18:44:58 -0800

    Passed Chatroom behavior tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,17 @@
-$('#submit').on('click', function() {
-  console.log('clicked')
-  var messageObj = {};
+// $('#submit').on('click', function() {
+//   console.log('clicked')
+//   var messageObj = {};
 
-  // messageObj.username = window.location.search.split('=')[1];
+//   // messageObj.username = window.location.search.split('=')[1];
 
-  messageObj.text = $('#message').val();
+//   messageObj.text = $('#message').val();
 
-  messageObj.roomname = '4chan';
+//   messageObj.roomname = '4chan';
 
-  console.log(messageObj);
+//   console.log(messageObj);
 
-  return messageObj;
-});
+//   return messageObj;
+// });
 
 // $('button').on('click', function() {
 // })
@@ -20,15 +20,10 @@ var app = $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
-    data: 'text',
+    data: '',
     contentType: 'application/json',
     success: function (data) {
-      console.dir(data)
-      var chats = data.results;
-
-      for (var i = 0; i < chats.length; i++) {
-        $('<div id="&amp&lt&gt' + chats[i].username + '"><span class="chat username">@' + chats[i].username + ': </span>' + chats[i].text + '</div>').appendTo('#chats');
-      }
+      console.dir(data);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -57,7 +52,57 @@ app.send = function(message) {
   
 }
 
-app.fetch = function() {
+app.fetch = function(message) {
+  return $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: message,
+    type: 'GET',
+    data: '',
+    contentType: 'application/json',
+    success: function (data) {
+      console.dir(data)
+      // app.addMessage(data)
 
+      for (var i = 0; i < chats.length; i++) {
+        $('<div id="&amp&lt&gt' + chats[i].username + '"><span class="chat username">@' + chats[i].username + ': </span>' + chats[i].text + '</div>').appendTo('#chats');
+      }
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get messages');
+    }
+  })
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
 }
+
+app.addMessage = function(message) {
+  $('<div><span class="chat username">@' + message.username + ': </span>' + message.text + '</div>').appendTo('#chats');
+}
+
+app.addRoom = function(roomName) {
+  $('<option>' + roomName + '</option>').appendTo('#roomSelect');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
